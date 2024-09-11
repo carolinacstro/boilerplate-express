1e54e6bf57b2ac5d81fc4198f938a6d92d906739
carolinacstro <carolinacoliveira@outlook.com>
2024-09-10 21:53:56 -0300

    basic node and express

--- myApp.js
+++ myApp.js
@@ -1,39 +1,28 @@
-let express = require('express');
+let express = require("express");
+const res = require("express/lib/response");
 let app = express();
+let path = require("path");
+require("dotenv").config();
 
+console.log("Hello World");
 
+// Middleware para servir arquivos estáticos
+app.use("/public", express.static(__dirname + "/public"));
 
+// Rota para a página inicial
+app.get("/", function (req, res) {
+  let absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+app.get("/json", function (req, res) {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
+});
 
+module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
